b16f1ef2bbd04e575c87c21ed769a65268b7463d
Omar Guajardo <omarguajardo@fb.com>
2020-06-24 17:00:52 -0700

    Implemented a Movie class that will be hold the title, posterPath image and overview of the movies we received from the API

--- app/src/main/java/com/example/flix/MainActivity.java
+++ app/src/main/java/com/example/flix/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.flix;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=fb1cb576c71896da8c7c626bae047420";
+    final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG,"onSucces");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG,"Results " + results.toString());
+                } catch (JSONException e) {
+                    Log.e(TAG,"Hit JSON Exception",e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+
+                Log.i(TAG,"onFailure");
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/flix/models/Movie.java
@@ -0,0 +1,29 @@
+package com.example.flix.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String title;
+    String posterPath;
+    String overView;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.title = jsonObject.getString("title");
+        this.posterPath = jsonObject.getString("poster_path");
+        this.overView = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws  JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+}
