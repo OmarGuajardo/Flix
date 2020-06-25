317f8d0848920b3dbb99978d792015624ac6316a
Omar Guajardo <omarguajardo@fb.com>
2020-06-24 19:21:31 -0700

    Made an adapter 'MovieAdapter' and referenced the correspondent view Items

---
+++ app/src/main/java/com/example/flix/adapters/MovieAdapter.java
@@ -0,0 +1,29 @@
+package com.example.flix.adapters;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flix.R;
+
+public class MovieAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvOverView;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverView = itemView.findViewById(R.id.tvOverView);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+    }
+
+
+}
