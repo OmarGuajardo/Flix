635b3ca998323bd8717f08314bdeca57183de660
Omar Guajardo <omarguajardo@fb.com>
2020-06-24 19:34:43 -0700

    Created MovieAdapter and constructed basic methods to populate the View with the ViewHolders, also incorporated the Libary Glide to render an image using an url

--- app/src/main/java/com/example/flix/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flix/adapters/MovieAdapter.java
@@ -1,15 +1,57 @@
 package com.example.flix.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.flix.R;
+import com.example.flix.models.Movie;
 
-public class MovieAdapter {
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    //This will inflate a layout from XML and return the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View movieView = inflater.inflate(R.layout.item_movie,parent,false);
+        return new ViewHolder(movieView);
+    }
+
+
+    //Populating data to the View THROUGH the ViewHolder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //Get the movie at the correct position
+        Movie selectedMovie = movies.get(position);
+
+        //Bind the movie data into the ViewHolder
+        holder.bind(selectedMovie);
+    }
+
+    //Return the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -23,6 +65,13 @@ public class MovieAdapter {
             tvOverView = itemView.findViewById(R.id.tvOverView);
             ivPoster = itemView.findViewById(R.id.ivPoster);
         }
+
+        public void bind(Movie selectedMovie) {
+             tvTitle.setText(selectedMovie.getTitle());
+             tvOverView.setText(selectedMovie.getOverView());
+             Glide.with(context).load(selectedMovie.getPosterPath()).into(ivPoster);
+        }
+
     }
 
 
