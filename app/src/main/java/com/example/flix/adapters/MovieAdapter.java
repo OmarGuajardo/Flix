6fb10fc0c2304fbb06e1aabcc02f1bb8a34cb016
Omar Guajardo <omarguajardo@fb.com>
2020-06-25 18:40:44 -0700

    used the Glide library to re-design the main UI

--- app/src/main/java/com/example/flix/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flix/adapters/MovieAdapter.java
@@ -14,6 +14,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.flix.MovieDetailsActivity;
 import com.example.flix.R;
 import com.example.flix.models.Movie;
@@ -22,6 +24,8 @@ import org.jetbrains.annotations.NotNull;
 import org.parceler.Parcels;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
@@ -89,7 +93,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             else{
                 imageUrl = selectedMovie.getPosterPath();
             }
-             Glide.with(context).load(imageUrl).into(ivPoster);
+             Glide.with(context)
+                     .load(imageUrl)
+                     .circleCrop()
+                     .into(ivPoster);
         }
 
 
