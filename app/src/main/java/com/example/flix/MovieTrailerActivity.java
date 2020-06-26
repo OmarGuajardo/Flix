8d64420204ea154755ca7fa7e4d9ccd45c91b854
Omar Guajardo <omarguajardo@fb.com>
2020-06-26 09:31:27 -0700

    Initiliazed the YouTube Player in MovieTrailerActivity.java

--- app/src/main/java/com/example/flix/MovieTrailerActivity.java
+++ app/src/main/java/com/example/flix/MovieTrailerActivity.java
@@ -3,12 +3,45 @@ package com.example.flix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MovieTrailerActivity extends AppCompatActivity {
+import com.example.flix.databinding.ActivityMovieDetailsBinding;
+import com.example.flix.databinding.ActivityMovieTrailerBinding;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MovieTrailerActivity extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Incorporating the bind library
+        ActivityMovieTrailerBinding binding = ActivityMovieTrailerBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_trailer);
+        setContentView(binding.getRoot());
+
+        // temporary test video id -- TODO replace with movie trailer video id
+        final String videoId = "tKodtNFpzBA";
+
+        // resolve the player view from the layout
+        YouTubePlayerView playerView = (YouTubePlayerView)binding.player;
+
+        // initialize with API key stored in secrets.xml
+        playerView.initialize(getString(R.string.API_KEY), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                YouTubePlayer youTubePlayer, boolean b) {
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(videoId);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                YouTubeInitializationResult youTubeInitializationResult) {
+                // log the error
+                Log.e("MovieTrailerActivity", "Error initializing YouTube player");
+            }
+        });
     }
 }
